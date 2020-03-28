469e81c58cc074b9e8e4263b83447968c3529f04
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-03-29 00:41:09 +0100

    ft: finalize the initial data display

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,20 @@
 window.onload = function () {
   chrome.storage.sync.get(['no-comment-preferences'], function (items) {
     var settings = items['no-comment-preferences'];
-    console.log(settings, );
 
-    // if (settings.isEnabled) {
-    //   $('all-sites').attr('checked', true);
-    // } else {
-    //   $('all-sites').attr('disabled', true);
-    //   $('youtube').attr('disabled', true);
-    //   $('facebook').attr('disabled', true);
-    // }
+    if (settings.isEnabled) {
+      var youtube = $('#youtube');
+      var facebook = $('#facebook');
+
+      // enable the checkboxes
+      youtube.attr('disabled', false);
+      facebook.attr('disabled', false);
+
+      // determine which options are enabled
+      youtube.attr('checked', settings.isYoutubeEnabled);
+      facebook.attr('checked', settings.isFacebookEnabled);
+    } else {
+      $('#all-sites').attr('checked', false);
+    }
   });
 };
