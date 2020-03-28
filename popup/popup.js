2c8ce4a0a15cf034b56b6d684a5ccb4a70fb272a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-03-28 23:37:36 +0100

    ft: switch away from alpine again

--- popup/popup.js
+++ popup/popup.js
@@ -1,14 +1,14 @@
 window.onload = function () {
   chrome.storage.sync.get(['no-comment-preferences'], function (items) {
     var settings = items['no-comment-preferences'];
-    console.log(settings, $);
+    console.log(settings, );
 
-    if (settings.isEnabled) {
-      $('all-sites').attr('checked', true);
-    } else {
-      $('all-sites').attr('disabled', true);
-      $('youtube').attr('disabled', true);
-      $('facebook').attr('disabled', true);
-    }
+    // if (settings.isEnabled) {
+    //   $('all-sites').attr('checked', true);
+    // } else {
+    //   $('all-sites').attr('disabled', true);
+    //   $('youtube').attr('disabled', true);
+    //   $('facebook').attr('disabled', true);
+    // }
   });
 };
