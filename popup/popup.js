6a1f160805960fb64bd4fe2d5b5a24c00046c7ee
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-03-29 15:11:24 +0100

    ft: sybc youtube settings

--- popup/popup.js
+++ popup/popup.js
@@ -8,14 +8,14 @@ window.onload = function () {
   chrome.storage.sync.get(['no-comment-preferences'], (items) => {
     settings = items[settingsKey];
 
+    // determine which options are enabled
+    youtube.attr('checked', settings.isYoutubeEnabled);
+    facebook.attr('checked', settings.isFacebookEnabled);
+
     if (settings.isEnabled) {
       // enable the checkboxes
       youtube.attr('disabled', false);
       facebook.attr('disabled', false);
-
-      // determine which options are enabled
-      youtube.attr('checked', settings.isYoutubeEnabled);
-      facebook.attr('checked', settings.isFacebookEnabled);
     } else {
       allSites.attr('checked', false);
     }
@@ -33,10 +33,22 @@ allSites.change(function () {
   facebook.attr('disabled', !isEnabled);
 
   // update the sync the settings
+  syncSettings();
+});
+
+youtube.change(function () {
+  var youtubeEnabled = this.checked;
+  settings.isYoutubeEnabled = youtubeEnabled;
+
+  // sync settings
+  syncSettings();
+});
+
+function syncSettings() {
   console.log(settings);
   var data = {};
   data[settingsKey] = settings;
   chrome.storage.sync.set(data, function () {
     console.log('plugin successfully disabled.');
   });
-});
+}
