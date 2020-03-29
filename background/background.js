450d2eb84c6977261d2deb830fcf3a529471e3d0
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-03-29 18:42:37 +0100

    ft: update background process

--- background/background.js
+++ background/background.js
@@ -1,6 +1,6 @@
 var settingsKey = 'no-comment-preferences';
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
   seedExtensionSettings();
 });
 chrome.runtime.onStartup.addListener(function () {
@@ -23,3 +23,12 @@ function seedExtensionSettings() {
     }
   })
 }
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostContains: 'facebook.com'}
+      })], actions: [new chrome.declarativeContent.ShowPageAction()]
+  }])
+});
