c2bf6d325dfc286253b0168ff8f2d4913fed402f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-04-11 20:50:48 +0100

    ft: add logic to sync changes with actions

--- content/content.js
+++ content/content.js
@@ -13,3 +13,25 @@ function removeYoutubeComments() {
 function restoreYoutubeComments() {
 
 }
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.isEnabled) {
+    if (request.isYoutubeEnabled) {
+      removeYoutubeComments();
+    } else {
+      restoreYoutubeComments();
+    }
+
+    if (request.isFacebookEnabled) {
+      removeFacebookComments();
+    } else {
+      restoreFacebookComments();
+    }
+  } else {
+    restoreFacebookComments();
+    restoreYoutubeComments();
+  }
+
+  sendResponse({});
+});
